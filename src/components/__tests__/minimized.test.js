3e8ec8f9cd53dc7b9ddd981915329996a87b0a86
Thomas Joußen <tjoussen91@gmail.com>
2020-01-05 20:30:17 +0100

    fix: import statement in minimized tests

--- src/components/__tests__/minimized.test.js
+++ src/components/__tests__/minimized.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from "react-test-renderer";
-import Minimized from "../Minimized";
+import Minimized from "../minimized";
 
 describe('Minimized', () => {
     beforeAll(() => {
